97143e08a31d6a779ac77c77c3b7e1a63f9bc330
jcarcell94 <65658608+jcarcell94@users.noreply.github.com>
2020-08-12 23:26:40 +0100

    Update LotterieStorage.sol
    - Añadido comentarios en formato Natspec
    - Eventos cuando se crea el LotteryStorage y cuando se cambía la versión de la LotteryFactory.
    - Implementado mecanismo para el cambio de versiones de LotteryFactory, solo la versi.on actual de factoria puede cambiar a una nueva versión.
    - Modificador OnlyFactory
    - Función fallback que no acepta Ether.

--- LotterieStorage.sol
+++ LotterieStorage.sol
@@ -1,22 +1,55 @@
 pragma solidity ^0.5.11;
 
+/**
+* @title LotterieStorage
+* @dev El contrato LotterieStorage implementa la parte del almacenamiento de las loterias
+* Sus funciones permiten modificar la lista de loterias, devolver la misma y proporciona un
+* un mecanismo para que solo la ultima version de lotterieFactory use el LotterieStorage.
+*/
+
 contract LotterieStorage{
     
     address[] public lotteries;
-    address storageDir;
+    address factoryAddress;
+    
+    // Eventos
+    event FactoryChanged(address indexed previousOwner, address indexed newOwner);
+    event LotterieStorageCreated(address indexed lotterieFactory, address indexed lotterieStorage);
     
     constructor() public {
+        factoryAddress = msg.sender;
+        emit LotterieStorageCreated(factoryAddress, address(this));
     }
     
-    function addLotterie(address _lot) public {
+    modifier onlyFactory() {
+        require(msg.sender == factoryAddress, 'Only last factory version');
+        _;
+    }
+    
+    function addLotterie(address _lot) onlyFactory public {
         lotteries.push(_lot);
     }
     
-    function getLotteries() public view returns(address[] memory){
+    function getLotteries() public view onlyFactory returns(address[] memory){
         return lotteries;
     }
     
-    function getDir() public view returns(address _dir){
+    function getDir() public view onlyFactory returns(address _dir){
         return address(this);
     }
+    
+    function changeFactory(address newFactory) public onlyFactory {
+        _changeFactory(newFactory); 
+    }
+    
+    function _changeFactory(address newFactory) internal {
+        require(factoryAddress != address(0), "Ownable: new owner is the zero address"); emit FactoryChanged(factoryAddress, newFactory);
+        factoryAddress = newFactory;
+    }
+    
+    // Fallback function
+    function() external payable{
+        require(msg.value == 0, 'Este contrato no acepta Ether');
+    }
+    
 }
